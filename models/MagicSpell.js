e70af8189586e2f7587afc7267e44024a9236451
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2021-11-29 13:40:03 +0100

    schemas: tasks 3-9

--- models/MagicSpell.js
+++ models/MagicSpell.js
@@ -1,16 +1,21 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const magicSpellSchema = new Schema({
-    name: String,
-    description: String,
-    cost: Number,
-    damage: Number,
-    castingTime: Number,
-    level: Number,
-    range: Number,
-    areaOfEffect: Boolean
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  cost: { type: Number, min: 1 },
+  damage: { type: Number, required: true, min: 0 },
+  castingTime: { type: Number, default: 6, min: 0 },
+  level: { type: Number, default: 1, min: 1 },
+  range: { type: Number, default: 0.1, min: 0 },
+  areaOfEffect: { type: Boolean, default: false },
+  school: {
+    type: String,
+    enum: ["physical", "arcane", "fire", "frost", "nature", "shadow", "holy"],
+  },
+  required: true,
 });
 
-const MagicSpell = model('MagicSpell', magicSpellSchema);
+const MagicSpell = model("MagicSpell", magicSpellSchema);
 
 module.exports = MagicSpell;
--- models/Wizard.js
+++ models/Wizard.js
@@ -1,14 +1,50 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const wizardSchema = new Schema({
-    name: String,
-    description: String,
-    age: Number,
-    level: Number,
-    accuracy: Number,
-    critical: Number,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  age: { type: Number, required: true, min: 1 },
+  level: { type: Number, default: 1, min: 1 },
+  accuracy: { type: Number, default: 0.3, min: 0, max: 1 },
+  critical: { type: Number, default: 0, min: 0, max: 1 },
+  origin: {
+    type: String,
+    enum: [
+      "Ankh-Morpork",
+      "Bad Schüschein",
+      "Betrek",
+      "Borogravia",
+      "Brindisi",
+      "Chimeria",
+      "Chirm",
+      "Copperhead",
+      "Cori Celesti",
+      "Djelibeybi",
+      "Ephebe",
+      "Ghat",
+      "Hersheba",
+      "Howondaland",
+      "Klatchistan",
+      "Lipwig",
+      "Rehigreed Province",
+      "Schmaltzberg",
+      "Skund",
+      "Sto Helit",
+      "Sto Lat",
+      "Sunken Leshp",
+      "The Chalk",
+      "The Wyrmberg",
+      "Überwald",
+    ],
+    required: true,
+  },
+  energy: {
+    health: { type: Number, default: 1, min: 0 },
+    stamina: { type: Number, default: 10, min: 0 },
+    magic: { type: Number, default: 100, min: 0 },
+  },
 });
 
-const Wizard = model('Wizard', wizardSchema);
+const Wizard = model("Wizard", wizardSchema);
 
 module.exports = Wizard;
